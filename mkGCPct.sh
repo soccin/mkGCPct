c3b9cdf0a27ccac60252e2a3ba870653e4b1cad0
Nicholas D. Socci <soccin@mskcc.org>
2018-11-29 20:17:17 -0500

    Added usage, removed bsubs, make genome subdir

--- mkGCPct.sh
+++ mkGCPct.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # /ifs/depot/assemblies/M.musculus/mm9/mm9.fasta
+
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != "1" ]; then
+    echo "usage: mkGCPct.sh GEOME.FASTA"
+    exit
+fi
+
 GENOME_FASTA=$1
 GENOME_BUILD=$(echo $GENOME_FASTA | perl -ne 'm|/([^/]*?).fa|; print $1')
 
@@ -8,13 +16,12 @@ echo GENOME_FASTA=$GENOME_FASTA
 echo GENOME_BUILD=$GENOME_BUILD
 
 if [ ! -e "genomes/${GENOME_BUILD}.genome" ]; then
+    mkdir -p genomes
     cat ${GENOME_FASTA}.fai | cut -f1,2 | fgrep -v _ | fgrep -vi decoy >genomes/${GENOME_BUILD}.genome
     echo "Building bedtools genome file [${GENOME_BUILD}.genome]"
 fi
 
-bsub -o LSF/ -J MKNUC_$$ -R "rusage[mem=60]" -n 3 \
-    ./mkNucFile.sh genomes/${GENOME_BUILD}.genome $GENOME_FASTA
+$SDIR/mkNucFile.sh genomes/${GENOME_BUILD}.genome $GENOME_FASTA
 
-bsub -o LSF/ -J MKRDA_$$ -We 59 -R "rusage[mem=60]" -w "post_done(MKNUC_$$)" -n 3 \
-    Rscript --no-save mkRDA.R ${GENOME_BUILD}_1000by100_TileBin.nuc.gz
+Rscript --no-save mkRDA.R ${GENOME_BUILD}_1000by100_TileBin.nuc.gz
 
